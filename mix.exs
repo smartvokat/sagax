a10c03733a1acde1f2b02ac8fe52e39d9563b707
Christian Schuhmann <madebyherzblut@users.noreply.github.com>
2020-04-30 00:16:39 +0200

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule Sagax.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :sagax,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+    ]
+  end
+end
