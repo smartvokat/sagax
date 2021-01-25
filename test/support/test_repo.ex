f3ed2070679f5aec33a26d7ece4b1258a82816cf
Damir Gainetdinov <damir.gainetdinov@smartvokat.com>
2021-01-25 14:36:46 +0100

    feat: add transaction support

---
+++ test/support/test_repo.ex
@@ -0,0 +1,13 @@
+defmodule TestRepo do
+  def transaction(fun, opts) do
+    send(self(), {:transaction, fun, opts})
+
+    {:ok, fun.()}
+  end
+
+  def rollback(error) do
+    send(self(), {:rollback, error})
+
+    {:error, error}
+  end
+end
