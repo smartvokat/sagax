8378d5e03141d79ae3be9abfa81b8add7aa79eaa
Christian Schuhmann <madebyherzblut@users.noreply.github.com>
2021-03-22 21:44:45 +0100

    feat: Add ex_docs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,17 @@ defmodule Sagax.MixProject do
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Sagax",
+      source_url: "https://github.com/smartvokat/sagax",
+      homepage_url: "https://github.com/smartvokat/sagax",
+      docs: [
+        main: "Sagax",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -26,8 +36,7 @@ defmodule Sagax.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
